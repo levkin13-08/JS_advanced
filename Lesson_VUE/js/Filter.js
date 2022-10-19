149aec59aaf9d7fb846494ea15d141661bd15dc1
Александр <102365356+levkin13-08@users.noreply.github.com>
2022-10-19 19:46:07 +0300

    lesson_6

---
+++ Lesson_VUE/js/Filter.js
@@ -0,0 +1,13 @@
+Vue.component('filter-el', {
+    data(){
+      return {
+          userSearch: ''
+      }
+    },
+    template: `<form action="#" class="search-form" @submit.prevent = "filter">
+                    <input type="text" class="search-field" v-model="userSearch">
+                    <button class="btn-search" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>`
+})
